c7d34b3614e1e8938deb282a5c64a681a62b51a8
Kent <kralorro@gmail.com>
2023-08-09 06:43:02 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,17 @@
+import configparser
+from libraries.database import PostgreSQL
+
+if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config.read('app-config.ini')
+    print(config['POSTGRES']['ip'])
+
+    test = PostgreSQL(
+        config['POSTGRES']['dbname'],
+        config['POSTGRES']['ip'],
+        config['POSTGRES']['username'],
+        config['POSTGRES']['password'],
+        config['POSTGRES']['port'])
+    sql = "select * from users"
+    print(test.execute_query(sql)[1])
+

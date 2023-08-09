00bd4df633ae7d78ad3f454975bbd0fd377f3260
kent <kralorro@gmail.com>
2023-08-09 07:59:16 -0400

    add crypto class

--- libraries/security.py
+++ libraries/security.py
@@ -1,15 +1,24 @@
+"""
+Author: Kent
+Name:   security.py
+Function
+    Class for data encryption
+"""
 from cryptography.fernet import Fernet
 
 class Cryptography:
     def __init__(self, key=False):
-        self.__key = Fernet.generate_key() if key is False else key.encode('UTF-8')
+        if key is False:
+            self.__key = Fernet.generate_key()
+        else:
+            self.__key = key.encode('UTF-8')
         self.__cipher_suite = Fernet(self.__key)
 
     def get_key(self):
         return self.__key.decode('UTF=8')
 
     def encrypt(self, clear_text):
-        return self.__cipher_suite.encrypt((clear_text).encode('UTF-8')).decode('UTF-8')
+        return self.__cipher_suite.encrypt(clear_text.encode('UTF-8')).decode('UTF-8')
 
     def decrypt(self, cypher_text):
         return self.__cipher_suite.decrypt(cypher_text.encode('UTF-8')).decode('UTF-8')
